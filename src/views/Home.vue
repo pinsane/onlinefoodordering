839ade96216cc47ccd1355ebd8adb2ad01f9fcbf
Farsid Mirza <farsid.mirza@lcp.uk.com>
2020-01-27 12:02:02 +0000

    init application

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <b-jumbotron header="BootstrapVue" lead="Online Food">
+      <p>For more information visit our website</p>
+      <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to food online" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
